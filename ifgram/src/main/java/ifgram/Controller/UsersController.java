e75ba25e51ec8b03f97a4262faf86a2d84920ef5
Deivid Leite da Costa Nunes <202412060011@ad.ifpb.edu.br>
2026-09-10 07:44:22 -0300

    Controller Métodos

--- ifgram/src/main/java/ifgram/Controller/UsersController.java
+++ ifgram/src/main/java/ifgram/Controller/UsersController.java
@@ -1,15 +1,29 @@
 package ifgram.Controller;
 
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("users")
 public class UsersController {
         @GetMapping
         public String getUser() {
-            return "get user was called";
+            return "Olá mundo";
         }
 
+        @PostMapping
+        public String postUser(){
+           return "chamei o endpoint como um POST" ;
+        }
+        @DeleteMapping
+            public String deleteUser(){
+            return "chamei o endpoint como um DELETE";
+        }
+        @PutMapping
+        public String putUser(){
+            return "chamei o endpoint como um PUT";
+        }
+        @PatchMapping
+        public String patchUser(){
+            return "chamei o endpoint como um PUT";
+        }
 }
