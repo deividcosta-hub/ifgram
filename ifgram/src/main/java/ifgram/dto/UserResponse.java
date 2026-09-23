2bb1c5d319eea9426f8fb8e791ffc56817c91b4e
Deivid Leite da Costa Nunes <202412060011@ad.ifpb.edu.br>
2026-09-23 09:50:56 -0300

    Response

--- ifgram/src/main/java/ifgram/dto/UserResponse.java
+++ ifgram/src/main/java/ifgram/dto/UserResponse.java
@@ -1,4 +1,8 @@
 package ifgram.dto;
 
-public record UserResponse() {
+public record UserResponse(Long id, String nome, String email) {
+    public static UserResponse from(User user) {
+            return new UserResponse(user.getId(), user.getNome(), user.getEmail()
+
+    }
 }
