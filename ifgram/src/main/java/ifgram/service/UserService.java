17b3a0a5f0db42c3430c8293a0d9688b30ef49ad
Deivid Leite da Costa Nunes <202412060011@ad.ifpb.edu.br>
2026-09-24 08:20:17 -0300

    UserService

--- ifgram/src/main/java/ifgram/service/UserService.java
+++ ifgram/src/main/java/ifgram/service/UserService.java
@@ -1,5 +1,29 @@
 package ifgram.service;
 
+import ifgram.dto.UserRequest;
+import ifgram.dto.UserResponse;
+import jakarta.transaction.Transactional;
+import org.springframework.stereotype.Service;
+
+@Service
 public class UserService {
+
+    private final UserRepository repository;
+
+    public UserService(UserRepository repository){
+        this.repository= repository;
+
+    }
+    @Transactional
+    public UserResponse criar(UserRequest request){
+        if (repository.existsByEmail(request.email())){
+            throw new EmailDuplicadoException(request,email());
+
+        }
+        User salvo= repository.save(new User(request.nome(), request.email()));
+        return UserResponse.from(salvo);
+
+
+    }
 
 }
